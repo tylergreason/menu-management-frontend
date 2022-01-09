5f563ccde7a9a1573be547001b7421e88d0553d1
Tyler Greason <59293587+tylergreason@users.noreply.github.com>
2022-01-09 11:12:43 -0500

    Added some styling to menu

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import {MenuItem} from '../../shared/MenuItem';
-
+import './Menu.css';
 
 function Menu() {
     return (
-        <>
-            <div>menu</div>
+        <div id="menu">
+            <h2>Restaurant Menu</h2>
         <div>
 
+        <div className="menu-items">
             <MenuItem></MenuItem>
             <MenuItem></MenuItem>
             <MenuItem></MenuItem>
             <MenuItem></MenuItem>
         </div>
-        </>
+        </div>
+        </div>
     );
 };
 
