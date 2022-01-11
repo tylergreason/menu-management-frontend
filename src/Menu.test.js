d1a3d980d9182729806392436cdd8f8b5b5f369c
Tyler Greason <59293587+tylergreason@users.noreply.github.com>
2022-01-11 17:42:39 -0500

    Test to find delete menu item button

--- src/Menu.test.js
+++ src/Menu.test.js
@@ -5,4 +5,12 @@ import Menu from './components/menu/Menu';
 it('renders menu items', () => {
 	mount(<Menu />);
 	cy.get('section#menu').should('be.visible');
+	cy.get('.menu-item').should('be.visible');
+});
+
+describe('Delete Menu Item', () => {
+	it('Menu item elements should have a delete button.', () => {
+		mount(<Menu />);
+		cy.get('.menu-item').find('.delete-button').should('be.visible');
+	});
 });
