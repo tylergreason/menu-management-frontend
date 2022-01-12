7d76b4ab585f4be6034a906273a874d617716f8b
Tyler Greason <59293587+tylergreason@users.noreply.github.com>
2022-01-12 16:03:03 -0500

    Added test for clicking "no" on delete modal

--- src/Menu.test.js
+++ src/Menu.test.js
@@ -9,24 +9,22 @@ it('renders menu items', () => {
 });
 
 describe('Delete Menu Item', () => {
-	it('Menu item elements should have a delete button.', () => {
+	beforeEach(() => {
 		mount(<Menu />);
+	});
+	it('Menu item elements should have a delete button.', () => {
 		cy.get('.menu-item').find('.delete-button').should('be.visible');
 	});
 
 	it("Clicking a menu item's delete button should bring up the delete confirmation modal.", () => {
-		mount(<Menu />);
 		cy.get('#delete-confirmation-modal').should('not.be.visible');
 		cy.get('#delete-confirmation-modal').should('exist');
 		cy.get('.menu-item .delete-button').first().click();
 		cy.get('#delete-confirmation-modal').should('not.have.class', 'hidden');
 	});
 
-	it('Confirming the deletion of a menu item should remove it from menuItemData and hide the modal', () => {
-		mount(<Menu />);
+	it('Confirming the deletion of a menu item should remove it from menuItemData and hide the delete modal.', () => {
 		const initialMenuItemQuantity = 20;
-		const menuItems = cy.get('.menu-item');
-		console.log(menuItems);
 		cy.get('.menu-item')
 			.its('length')
 			.should('eq', initialMenuItemQuantity);
@@ -37,6 +35,21 @@ describe('Delete Menu Item', () => {
 		cy.get('.menu-item')
 			.its('length')
 			.should('eq', initialMenuItemQuantity - 1);
+		cy.get('#delete-confirmation-modal').should('not.be.visible');
+	});
+
+	it('Choosing to not delete an item should not remove the item and should hide the delete modal.', () => {
+		const initialMenuItemQuantity = 20;
+		cy.get('.menu-item')
+			.its('length')
+			.should('eq', initialMenuItemQuantity);
+		cy.get('.menu-item .delete-button').first().click();
+		cy.get('#delete-confirmation-modal button:nth-of-type(2)').click({
+			force: true,
+		});
+		cy.get('.menu-item')
+			.its('length')
+			.should('eq', initialMenuItemQuantity);
 		cy.get('#delete-confirmation-modal').should('not.be.visible');
 	});
 });
