5b955dd3e74e9acfcb52de12f074db889d423089
Tyler Greason <59293587+tylergreason@users.noreply.github.com>
2022-01-12 16:09:45 -0500

    Moved tests to an appropriate folder

--- cypress/unit/Menu.test.js
+++ cypress/unit/Menu.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from '@cypress/react';
-import Menu from './components/menu/Menu';
+import Menu from '../../src/components/menu/Menu';
 
 it('renders menu items', () => {
 	mount(<Menu />);
