7471ad5c78ff3a25d2aa09a0c3e2f9aa57a5fcd8
Tyler Greason <59293587+tylergreason@users.noreply.github.com>
2022-01-13 15:54:02 -0500

    Setup test descriptions and first test

---
+++ cypress/unit/MenuItem.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { mount } from '@cypress/react';
+import Menu from '../../src/components/menu/Menu';
+
+const newMenuItemFieldNames = ['name', 'description', 'price', 'imgUrl'];
+
+// describe edit menu item button
+describe('Edit menu item button', () => {
+	it('Should exist.', () => {
+		cy.get('.menu-item .edit-button').to('exist');
+	});
+});
+// it('Should show the edit form of the menu item when clicked.', () =>{})
+
+// describe edit form
+// it('Should have fields for name, description, price, and imgUrl.', () =>{})
+
+// describe cancel button
+// it('Should exist.', () =>{})
+// it('Should hide the edit form when clicked.', () =>{})
+// it('Should revert the menu item back to its pre-editing state', () => {})
+
+// describe submit button
+// it('Should exist.', () =>{})
+// it('Should hide the edit menu form', () =>{})
+
+// describe editing menu items
+// it('Should edit the menu item immediately, visible in the DOM', () => {})
